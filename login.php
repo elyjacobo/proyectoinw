3b4a25bfba5ee3f656a08b8a2646f48e27d875e3
elyjacobo <elyjacobo@alswblog.org>
2013-07-02 04:17:03 -0600

    retomando el login

--- login.php
+++ login.php
@@ -1,34 +1,38 @@
-<?php 
-session_start();
-session_destroy();
-SESSION_START(); 
-$_SESSION['validador']='login';
-?>
-
-<!DOCTYPE HTML>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-<header>
-	<title>Gestion de fichas: login</title>
-	<link rel="stylesheet" href="css/login.css">
-</header>
-<body>
-	<section class="loginform cf">  
-	<form name="login" action="validarusuario.php" method="get" accept-charset="utf-8">  
-    <table>  
-        <tr>
-			<td><label for="username">Usuario:</label></td>
-			<td><input type="text" name="username" placeholder="text" required></td>
-		</tr> 
-        <tr>
-			<td><label for="password">Contrase&ntilde;a:</label></td>
-			<td><input type="password" name="password" placeholder="password" required></td>
-		</tr>
-    </table>
-	<input type="submit" value="Login"> 
-</form>  
-</section> 
-
-
-</body>
-</html> 
-	
+    <head>
+        <title>Ficha Tutoria Estudiantil: Login</title>
+        <link rel="stylesheet" type="text/css" href="css/login.css" />
+		<link type="text/css" href="css/ui-lightness/jquery-ui-1.8.24.custom.css" rel="stylesheet" />
+		<script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
+		<script type="text/javascript" src="js/jquery-ui-1.8.24.custom.min.js"></script>
+    </head>
+    <body>
+        <div id="login">
+        <center>
+            <?php
+                include('class/usuario.php');
+                Usuario::EstablecerZonaHoraria();
+                if(isset($_GET['error'])){
+                    Usuario::EscribirError($_GET['error']);
+                }
+            ?>
+            <h1>Login</h1>
+            <h3>Ingrese sus datos por favor:</h3>
+            <form method="POST" action="comprobarUsuario.php">
+                <table>
+                    <tr>
+                        <td><label for="user">Usuario: </label></td>
+                        <td><input type="text" name="usuario" size="25" maxlength="25"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="pass">Contrase&ntilde;a: </label></td>
+                        <td><input type="password" name="pass" size="25" maxlength="25"></td>
+                    </tr>
+                </table>
+                <input type="submit" value="Ingresar">
+            </form>
+        </center>
+            </div>
+    </body>
+</html>
